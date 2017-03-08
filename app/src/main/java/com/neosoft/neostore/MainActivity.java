96e19bf1783e5cda23f020872d58c0df6497b4a7
mayurikadam234 <mayuri.kadam@neosofttech.com>
2017-03-08 20:37:01 +0530

    login form with datav binding

--- app/src/main/java/com/neosoft/neostore/MainActivity.java
+++ app/src/main/java/com/neosoft/neostore/MainActivity.java
@@ -1,13 +1,24 @@
 package com.neosoft.neostore;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.neosoft.neostore.databinding.ActivityMainBinding;
+
+import com.neosoft.neostore.model.User;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.setMyUser(new User(null,"mayuri123"));
+
+        binding.txtUserName.setText("");
+
     }
 }
--- app/src/main/java/com/neosoft/neostore/model/User.java
+++ app/src/main/java/com/neosoft/neostore/model/User.java
@@ -9,6 +9,11 @@ public class User {
     private  String userName;
     private  String password;
 
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
     public String getUserName() {
         return userName;
     }
