dd5c2bc9c7aa50a9e0b42bd5b280e34105df8a84
nidhimodi <nidhi.modi@neosofttech.com>
2017-03-08 18:45:10 +0530

    Added toolbar in the activity layout

--- app/src/main/java/com/neosoft/neostore/MainActivity.java
+++ app/src/main/java/com/neosoft/neostore/MainActivity.java
@@ -1,7 +1,9 @@
 package com.neosoft.neostore;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+
+		Toolbar toolbar = (Toolbar ) findViewById( R.id.toolbar);
+		setSupportActionBar(toolbar);
+		ActionBar actionBar = getSupportActionBar();
+		if(actionBar != null)
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 }
